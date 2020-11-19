f0a5c537a27c7cffa811fe1d9d56f4c06ab2c222
ibra4 <ibra16@itc.hu.edu.jo>
2020-11-19 21:39:14 +0200

    Remove settings

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::name('settings')->middleware('can:manage-website')->get('/settings', [App\Http\Controllers\SettingsController::class, 'index'])->name('home');
